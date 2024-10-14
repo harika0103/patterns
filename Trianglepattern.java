66a2aec5e193d35de15ac32dbcd0b99954c51a2f
harika0103 <121282090+harika0103@users.noreply.github.com>
2024-10-14 20:46:35 +0530

    Update Trianglepattern.java

--- Trianglepattern.java
+++ Trianglepattern.java
@@ -2,7 +2,7 @@ class Trianglepattern {
     public static void main(String[] args) {
         for(int i=1;i<=6;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
