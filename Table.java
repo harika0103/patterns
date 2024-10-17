bd00d870b96a5867519de108c8278ad57f32de97
harika0103 <121282090+harika0103@users.noreply.github.com>
2024-10-17 19:36:27 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -2,7 +2,7 @@ public class Table{
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
             for(int j=1;j<=10;j++){
-                System.out.println(i+"*"+j+"="+i*j);
+                 System.out.print(j+"*"+i+"="+(i*j)+"\t");
             }
             System.out.println();
         }  
